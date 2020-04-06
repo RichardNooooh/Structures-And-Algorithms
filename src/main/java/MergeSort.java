e1cadf5ed7b4a859434aa331a685f1dc68d0bca6
Ice <richard.jun.noh@gmail.com>
2020-04-06 10:38:32 -0500

    Add base for MergeSort algorithm

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class MergeSort
+{
+	List<Comparable> list;
+	public MergeSort(List<Comparable> list)
+	{
+		this.list = list;
+		sort();
+	}
+
+	public List getSortedList()
+	{
+		return list;
+	}
+
+
+	private void sort()
+	{
+
+	}
+
+	private List<Comparable> merge(List<Comparable> list1, List<Comparable> list2)
+	{
+		return null;
+	}
+
+}
